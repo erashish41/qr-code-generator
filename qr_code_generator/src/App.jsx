a3ce55ffbcbf1ba361da05ddc3dfc7c0347b8b0f
Ashish Bhardwaj <erashish41@gmail.com>
2024-09-22 16:47:16 +0530

    qr code generator

---
+++ qr_code_generator/src/App.jsx
@@ -0,0 +1,11 @@
+import { QrGenerator } from "./components/QrGenerator"
+
+const App = () => {
+  return (
+    <div>
+      <QrGenerator />
+    </div>
+  )
+}
+
+export default App;
---
+++ qr_code_generator/src/components/QrGenerator.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Card } from "./Card";
+import { QRCode } from "react-qr-code";
+
+export const QrGenerator = () => {
+
+    const [qrCode, setQrCode] = useState("");
+    const [input, setInput] = useState("");
+
+    const handleQrCodeGenerator = () => {
+        setQrCode(input);
+        setInput("")
+    }
+
+    return (
+        <div className="qr-container">
+            <h1>Qr code Generator</h1>
+            <div className="">
+                <input 
+                    onChange={(e) => setInput(e.target.value)}
+                    type="text" 
+                    name="text"
+                    value={input}
+                    placeholder="enter your data"/>
+                <button
+                    disabled={input && input.trim() !== "" ? false : true}
+                     onClick={handleQrCodeGenerator}>Generator</button>
+            </div>
+            <div className="qr-code">
+                <QRCode 
+                    id="qr-value-code"
+                    value={qrCode}
+                    size={400}
+                    bgColor= "#fff"
+                />
+            </div>
+        </div>
+    )
+}
